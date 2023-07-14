5da031e975429cddf708b1fae8801f600f6aa367
Pawaret Dilokwuttisit <pawaret.dilok@gmail.com>
2023-07-15 01:53:45 +0700

    init Layer class

---
+++ src/main/java/com/ci/HiddenLayer.java
@@ -0,0 +1,4 @@
+package com.ci;
+
+public class HiddenLayer {
+}
---
+++ src/main/java/com/ci/MLP.java
@@ -0,0 +1,11 @@
+package com.ci;
+
+import com.ci.HiddenLayer;
+import com.ci.OutputLayer;
+
+import java.util.List;
+
+public class MLP {
+    private List<HiddenLayer> hiddenLayerList;
+    private OutputLayer outputLayer;
+}
